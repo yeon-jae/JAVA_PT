0137f28c278230a0205d6b5e065f719d3356da2f
yeon-jae <68237481+yeon-jae@users.noreply.github.com>
2023-01-11 16:30:43 +0900

    project_setting

---
+++ src/components/common/Button.js
@@ -0,0 +1,22 @@
+import { getByPlaceholderText } from '@testing-library/react';
+import styled, { withTheme } from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const StyledButton =styled.button`
+    border:none;
+    border-radius:4px;
+    font-size:1rem;
+    font-weight:bold;
+    padding:0.25rem 1rem;
+    color:white;
+    outline:none;
+    cursor:pointer;
+
+    background-color:${palette.gray[8]};
+    &:hover{
+        background:${palette.gray[6]}
+    }
+    `;
+
+    const Button = props =><StyledButton{...props}/>;
+    export default Button;
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,4 +1,10 @@
+import Button from "../components/common/Button";
+
 const PostListPage=()=>{
-    return <div>포스트 리스트</div>;
+    return(
+        <div>
+            <Button>버튼</Button>
+        </div>
+    );
 };
 export default PostListPage;
