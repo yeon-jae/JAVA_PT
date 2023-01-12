5b4a0aeaaa410204bbedd53483a6308a76cec8f0
yeon-jae <68237481+yeon-jae@users.noreply.github.com>
2023-01-12 23:44:10 +0900

    auth/first

---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+/*회원가입 또는 로그인 폼 보여주기*/
+
+const AuthFormBlock=styled.div``;
+const AuthForm=()=>{
+    return(
+        <AuthFormBlock>
+            AuthForm
+        </AuthFormBlock>
+    );
+};
+
+export default AuthForm;
---
+++ src/components/auth/AuthTemplate.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+/*회원가입 로그인 페이지의 레이아웃 담당하는 컴포넌트 */
+const AuthTemplateBlock=styled.div``;
+
+const AuthTemplate=()=>{
+    return(
+        <AuthTemplateBlock>
+
+        </AuthTemplateBlock>
+    );
+};
+export default AuthTemplate;
