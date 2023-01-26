1be1a38ff6f5a7ac32e8a939802f26cad2c7156b
yeon-jae <68237481+yeon-jae@users.noreply.github.com>
2023-01-26 13:52:00 +0900

    hooks2

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,39 +1,38 @@
-import React,{useState,useEffect} from "react";
-const Info=()=>{
-    const [name,setName]=useState('');
-    const [nickname,setNickname]=useState('');
-    useEffect(()=>{
-        console.log('effect');
-        console.log(name);
-        return()=>{
-            console.log('cleanup');
-            console.log(name);
-        };
-    },[name]);
-    const onChangeName=e=>{
-        setName(e.target.value);
+import React,{useReducer} from "react";
+function reducer(state, action) {
+    return {
+      ...state,
+      [action.name]: action.value
     };
-
-    const onChangeNickName=e=>{
-        setNickname(e.target.value);
+  }
+   
+  const Info = () => {
+    const [state, dispatch] = useReducer(reducer, {
+      name: '',
+      nickname: ''
+    });
+    const { name, nickname } = state;
+    const onChange = e => {
+      dispatch(e.target);
     };
-
-    return(
+   
+    return (
+      <div>
         <div>
-            <div>
-                <input value={name} onChange={onChangeName} />
-                <input value={nickname} onChange={onChangeNickName} />
-            </div>
-            <div>
-                <div>
-                    <b>이름: </b>{name}
-                </div>
-                <div>
-                    <b>닉네임: </b>{nickname}
-                </div>
-            </div>
+          <input name="name" value={name} onChange={onChange} />
+          <input name="nickname" value={nickname} onChange={onChange} />
         </div>
+        <div>
+          <div>
+            <b>이름:</b> {name}
+          </div>
+          <div>
+            <b>닉네임: </b>
+            {nickname}
+          </div>
+        </div>
+      </div>
     );
-
-};
-export default Info;
+  };
+   
+  export default Info;
