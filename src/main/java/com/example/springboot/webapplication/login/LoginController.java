2f3e6c13d90196d6132e4c4fb1e7d2774435ae44
yeon-jae <lisanam99@naver.com>
2023-08-19 04:08:09 +0900

    login_authorized

--- src/main/java/com/example/springboot/webapplication/login/LoginController.java
+++ src/main/java/com/example/springboot/webapplication/login/LoginController.java
@@ -1,11 +1,26 @@
 package com.example.springboot.webapplication.login;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
+    //login
+    //GET,POST
+    //get요청에 대해서만 이 메서드가 작용할 수 있도록
+    @RequestMapping(value = "login",method = RequestMethod.GET)
     public String gotoLoginPage() {
         return "login";
+    }
+    //POST에 대해서만
+    @RequestMapping(value = "login",method = RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name,
+                                  @RequestParam String password, ModelMap model) {
+        model.put("name",name);
+        model.put("password",password);
+
+        return "welcome";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome Page</title>
+</head>
+<body>
+    <div>
+        <h1>welcome to my page!!!</h1>
+    </div>
+    <div>Your name: ${name}</div>
+    <div>Your password: ${password}</div>
+
+</body>
+</html>
