fba1d301ea0d87bde9454b5e556c2dbdf2b382b9
yeon-jae <lisanam99@naver.com>
2023-08-22 01:43:05 +0900

    spring_boot_security_with_h2

--- src/main/java/com/example/springboot/webapplication/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/springboot/webapplication/security/SpringSecurityConfiguration.java
@@ -1,14 +1,19 @@
-package com.in28minutes.springboot.myfirstwebapp.security;
+package com.example.springboot.webapplication.security;
 
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -21,28 +26,33 @@ public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
 
-        UserDetails userDetails1 = createNewUser("MOYA", "dummy");
-        UserDetails userDetails2 = createNewUser("ranga", "dummydummy");
-
-        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
-    }
-
-    private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder
                 = input -> passwordEncoder().encode(input);
 
         UserDetails userDetails = User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username(username)
-                .password(password)
+                .username("MOYA")
+                .password("dummy")
                 .roles("USER","ADMIN")
                 .build();
-        return userDetails;
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
+
+        return http.build();
+    }
 }
